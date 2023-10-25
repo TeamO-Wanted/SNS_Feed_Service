8110c348341b9dcaeea0c75b1fe07f539ebbfc0b
HyungCheolSim <sim34122@gmail.com>
2023-10-25 20:13:04 +0900

    first commit

---
+++ src/main/java/com/wanted/sns_feed_service/SnsFeedServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.sns_feed_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsFeedServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsFeedServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wanted/sns_feed_service/SnsFeedServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.sns_feed_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsFeedServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
