80b8d22d31f7d7aed0609c76bad808b04824963c
johan1103 <johan1103@naver.com>
2023-10-25 21:06:10 +0900

    Feat: create feed repository

---
+++ src/main/java/com/wanted/sns_feed_service/feed/repository/FeedRepository.java
@@ -0,0 +1,7 @@
+package com.wanted.sns_feed_service.feed.repository;
+
+import com.wanted.sns_feed_service.feed.entity.Feed;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedRepository extends JpaRepository<Feed,Long> {
+}
