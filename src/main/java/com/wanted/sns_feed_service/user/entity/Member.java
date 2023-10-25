02fdd483520b4178d5e7ef9a3f95a51efc5a3188
johan1103 <johan1103@naver.com>
2023-10-25 21:03:51 +0900

    Refactor: add builder annotation, unique config

--- src/main/java/com/wanted/sns_feed_service/user/entity/Member.java
+++ src/main/java/com/wanted/sns_feed_service/user/entity/Member.java
@@ -1,14 +1,24 @@
 package com.wanted.sns_feed_service.user.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
   @Id
   @GeneratedValue
   private Long id;
+  @Column(unique = true)
   private String account;
   private String password;
   private String email;
