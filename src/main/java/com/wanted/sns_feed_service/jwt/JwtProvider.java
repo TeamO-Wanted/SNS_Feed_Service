c57b4cae63a1f6ef926fd73b55700d2cc8cf15ea
cheorhyeon <r4560798@naver.com>
2023-10-26 20:39:18 +0900

    feat : jwt Provider complete

--- src/main/java/com/wanted/sns_feed_service/jwt/JwtProvider.java
+++ src/main/java/com/wanted/sns_feed_service/jwt/JwtProvider.java
@@ -1,11 +1,17 @@
 package com.wanted.sns_feed_service.jwt;
 import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
 
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import com.wanted.sns_feed_service.util.Ut;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 
 @Component
@@ -26,5 +32,44 @@ public class JwtProvider {
 		if (cachedSecretKey == null) cachedSecretKey = _getSecretKey();
 
 		return cachedSecretKey;
+	}
+
+	// 토큰에 들어갈 정보와 만료 시간을 받으면 토큰을 생성하는 메서드
+	public String genToken(Map<String, Object> claims, int seconds) {
+		long now = new Date().getTime();
+		Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
+
+		return Jwts.builder()
+			.claim("body", Ut.json.toStr(claims))
+			.setExpiration(accessTokenExpiresIn)
+			// 추후 알고리즘과 비밀키를 사용하여 서버가 서명했는지 검증함
+			.signWith(getSecretKey(), SignatureAlgorithm.HS512)
+			.compact();
+	}
+
+	// 토큰이 유효한지 검사하는 메서드
+	public boolean verify(String token) {
+		try {
+			Jwts.parserBuilder()
+				.setSigningKey(getSecretKey())
+				.build()
+				.parseClaimsJws(token);
+		} catch (Exception e) {
+			return false;
+		}
+
+		return true;
+	}
+
+	// 토큰으로부터 정보를 추출하는 메서드
+	public Map<String, Object> getClaims(String token) {
+		String body = Jwts.parserBuilder()
+			.setSigningKey(getSecretKey())
+			.build()
+			.parseClaimsJws(token)
+			.getBody()
+			.get("body", String.class);
+
+		return Ut.json.toMap(body);
 	}
 }
