9498b8833b07a80a1022afeb636ceac7e527087e
cheorhyeon <r4560798@naver.com>
2023-10-29 16:30:11 +0900

    chore: swagger update

--- src/main/java/com/wanted/sns_feed_service/member/controller/MemberController.java
+++ src/main/java/com/wanted/sns_feed_service/member/controller/MemberController.java
@@ -15,6 +15,7 @@ import com.wanted.sns_feed_service.member.service.MemberService;
 import com.wanted.sns_feed_service.rsData.RsData;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
@@ -33,12 +34,15 @@ public class MemberController {
 	@Data
 	public static class SignupRequest {
 		@NotBlank(message = "id를 입력해주세요")
+		@Schema(description = "사용자 계정", example = "user123")
 		private String account;
 		@NotBlank(message = "pw를 입력해주세요")
 		@Pattern(regexp = "^(?![0-9]{10,}$)(?!.*(.)\\1{2,}).{10,}$", message = "비밀번호는 10자 이상 입력해야 하며, 숫자로만 이루어 질 수 없으며 3회 이상 연속되는 문자를 사용할 수 없습니다.")
+		@Schema(description = "사용자 비밀번호 (10자 이상, 숫자만으로 구성 불가, 3회 이상 연속되는 문자 사용 불가)", example = "password123")
 		private String password;
 		@Email(message = "올바른 이메일 형식을 입력하세요")
 		@NotBlank(message = "이메일을 입력해주세요")
+		@Schema(description = "사용자 이메일", example = "user123@example.com")
 		private String email;
 	}
 
@@ -54,7 +58,8 @@ public class MemberController {
 			return RsData.of("F-1", errorMessages.get(0));
 		}
 		// 입력 이상 없다면 계정 가입
-		RsData rsData = memberService.join(signupRequest.getAccount(), signupRequest.getPassword(), signupRequest.getEmail());
+		RsData rsData = memberService.join(signupRequest.getAccount(), signupRequest.getPassword(),
+			signupRequest.getEmail());
 
 		return rsData;
 	}
