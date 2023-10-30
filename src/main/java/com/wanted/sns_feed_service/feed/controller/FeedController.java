639956c161867f7f06f3a8c2c332109ccd739c80
yoonnable <yoonnable@gmail.com>
2023-10-30 21:26:31 +0900

    게시물 refactor : 게시물 상세, 좋아요, 공유 swagger Operation 작성

--- src/main/java/com/wanted/sns_feed_service/feed/controller/FeedController.java
+++ src/main/java/com/wanted/sns_feed_service/feed/controller/FeedController.java
@@ -58,6 +58,7 @@ public class FeedController {
      * 피드 상세 조회
      */
     @GetMapping("/{id}")
+    @Operation(summary = "피드 상세 조회", description = "피드 상세 정보를 조회하고, viewCount가 1 증가합니다.")
     public ResponseEntity<FeedDetailResponse> findFeed(@PathVariable long id) {
         Feed feed = feedService.findById(id);
 
@@ -68,6 +69,7 @@ public class FeedController {
      * 피드 좋아요
      */
     @PutMapping("/like/{id}")
+    @Operation(summary = "피드 좋아요", description = "외부 서비스의 피드 좋아요 endpoint를 호출합니다. likeCount가 1 증가합니다.")
     public ResponseEntity<FeedDetailResponse> likeFeed(@PathVariable long id) {
         Feed feed = feedService.likeById(id);
 
@@ -78,6 +80,7 @@ public class FeedController {
      * 피드 공유
      */
     @PutMapping("/share/{id}")
+    @Operation(summary = "피드 공유", description = "외부 서비스의 피드 공유 endpoint를 호출합니다. shareCount가 1 증가합니다.")
     public ResponseEntity<FeedDetailResponse> shareFeed(@PathVariable long id) {
         Feed feed = feedService.shareById(id);
 
