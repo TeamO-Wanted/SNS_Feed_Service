d926aea90c1ac7aa3b5ca8a705ff54401495f126
johan1103 <johan1103@naver.com>
2023-11-04 01:04:45 +0900

    feat: init interceptor, mvc configurationC #29

---
+++ src/main/java/com/wanted/sns_feed_service/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package com.wanted.sns_feed_service.config;
+
+import com.wanted.sns_feed_service.interceptor.AuthorizationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+@RequiredArgsConstructor
+public class MvcConfig implements WebMvcConfigurer {
+  private final AuthorizationInterceptor authorizationInterceptor;
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry){
+    registry.addInterceptor(authorizationInterceptor).addPathPatterns("/authorization/**");
+  }
+}
---
+++ src/main/java/com/wanted/sns_feed_service/interceptor/AuthorizationInterceptor.java
@@ -0,0 +1,12 @@
+package com.wanted.sns_feed_service.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class AuthorizationInterceptor implements HandlerInterceptor {
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    return HandlerInterceptor.super.preHandle(request, response, handler);
+  }
+}
