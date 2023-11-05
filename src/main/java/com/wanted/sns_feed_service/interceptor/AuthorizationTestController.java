6127e980dd7cdfe114ab01f17ddd1810b101fb72
johan1103 <johan1103@naver.com>
2023-11-06 03:02:51 +0900

    feat: adjust LoginMember annotation in controller #30

--- src/main/java/com/wanted/sns_feed_service/interceptor/AuthorizationTestController.java
+++ src/main/java/com/wanted/sns_feed_service/interceptor/AuthorizationTestController.java
@@ -1,5 +1,7 @@
 package com.wanted.sns_feed_service.interceptor;
 
+import com.wanted.sns_feed_service.resolver.LoginMember;
+import com.wanted.sns_feed_service.resolver.LoginUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/authorization/test")
 public class AuthorizationTestController {
   @GetMapping
-  public String authorizationTest(){
-    return "ok";
+  public String authorizationTest(@LoginUser LoginMember loginMember){
+    return loginMember.getAccount();
   }
 }
