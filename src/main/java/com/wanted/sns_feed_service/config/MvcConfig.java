e4add7f676e07d52859433b9f7d259101962b277
johan1103 <johan1103@naver.com>
2023-11-06 03:15:18 +0900

    feat: add resovler in mvc logic

--- src/main/java/com/wanted/sns_feed_service/config/MvcConfig.java
+++ src/main/java/com/wanted/sns_feed_service/config/MvcConfig.java
@@ -1,18 +1,27 @@
 package com.wanted.sns_feed_service.config;
 
 import com.wanted.sns_feed_service.interceptor.AuthorizationInterceptor;
+import com.wanted.sns_feed_service.resolver.LoginUserResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
   private final AuthorizationInterceptor authorizationInterceptor;
+  private final LoginUserResolver loginUserResolver;
 
   @Override
   public void addInterceptors(InterceptorRegistry registry){
     registry.addInterceptor(authorizationInterceptor).addPathPatterns("/v1/authorization/**");
+  }
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    resolvers.add(loginUserResolver);
   }
 }
