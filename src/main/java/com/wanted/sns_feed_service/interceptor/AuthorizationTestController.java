da6684713de21654b6002634aa19769bfa4014c5
cheorhyeon <r4560798@naver.com>
2023-11-06 16:28:44 +0900

    refactor : @Parameter(hidden) 옵션으로 파라미터 받지 않도록 수정

--- src/main/java/com/wanted/sns_feed_service/interceptor/AuthorizationTestController.java
+++ src/main/java/com/wanted/sns_feed_service/interceptor/AuthorizationTestController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 @RestController
 @RequestMapping("/v1/authorization/test")
 public class AuthorizationTestController {
   @GetMapping
-  public String authorizationTest(@LoginUser LoginMember loginMember){
+  public String authorizationTest(@LoginUser @Parameter(hidden = true) LoginMember loginMember){
     return loginMember.getAccount();
   }
 }
